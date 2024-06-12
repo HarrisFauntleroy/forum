0c06ea99ce2c7b50f1928c387d74bdc449374d81
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-12 11:35:38 +1000

    Update moderator login process in moderators_spec.rb

--- spec/system/moderators_spec.rb
+++ spec/system/moderators_spec.rb
@@ -5,13 +5,11 @@ require 'rails_helper'
 describe 'Moderators', type: :system, js: true do
   let(:moderator) { create(:user, :moderator) }
 
-  before do
-    sign_in moderator
-  end
-
   it 'moderator logs in' do
-    visit root_path
-    expect(page).to have_text("Welcome #{moderator.username}!")
+    sign_in moderator
+
+    expect(page).to have_text(moderator.username)
+    find('#user-profile').click
     expect(page).to have_text('Moderator')
   end
 end
